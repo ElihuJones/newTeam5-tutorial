3c3d073c218aa4b0eafd7ca0d8ca28b4f66f9dde
SaganJackson <sagan.jacksonmoses@gmail.com>
2022-11-03 12:26:51 -0700

    fixed google books api cards

--- script.js
+++ script.js
@@ -5,15 +5,31 @@ search_btn.addEventListener("click", () => {
 
   fetch(API_URL)
     .then((res) => res.json())
-    .then(({ items: googleBookImgs }) => {
-      for (const image of googleBookImgs) {
-        const imgUrl = image.volumeInfo.imageLinks.smallThumbnail;
+    .then(({ items: googleBooks }) => {
+      for (const gbElement of googleBooks) {
+        const imgUrl = gbElement.volumeInfo.imageLinks.smallThumbnail;
+        const preview = gbElement.volumeInfo.previewLink;
+
+        const card = document.createElement("div");
+        card.setAttribute("class", card);
+        card.setAttribute(
+          "style",
+          "width: 11rem; border: solid #D3D3D3; border-radius: 5%; margin: 5px; ; padding: 5px;"
+        );
+
+        const anchorTag = document.createElement("a");
+        anchorTag.setAttribute("href", preview);
+
+        card.appendChild(anchorTag);
 
         const img = document.createElement("img");
         img.setAttribute("src", imgUrl);
-        img.setAttribute("alt", image.alt_description);
+        img.setAttribute("alt", gbElement.alt_description);
+        // img.setAttribute("id");
 
-        photos_container.appendChild(img);
+        anchorTag.appendChild(img);
+
+        bookCard_container.appendChild(card);
       }
     });
 });
